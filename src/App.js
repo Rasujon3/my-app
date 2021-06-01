60f8098dbc4507fc997cc589ec4941e1a2b704f7
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-06-02 03:23:27 +0600

    props
    props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './mycomponents/Hello'
+import Welcome from './mycomponents/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Hello age="28" name="Sujon"></Hello>
+      <Hello age="22" name="Rifat"></Hello>
+      <Hello age="23" name="Salif"></Hello>
+      <Hello age="24" name="Liya"></Hello>
+
+      <Welcome></Welcome>
+
     </div>
   );
 }
---
+++ src/mycomponents/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello(props){
+    return (
+        <h1>Hello {props.name} and Age:{props.age} I am from functional component</h1>
+    );
+}
+
+export default Hello
